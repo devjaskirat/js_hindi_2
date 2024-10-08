ce0463a70871422f932b221606cc0d2dc3774ad4
devjaskirat <ramghariajaskirat608@gmail.com>
2024-10-08 23:20:29 +0530

    Create datatypes_summar.js

---
+++ .devcontainer/01/01_basics/datatypes_summar.js
@@ -0,0 +1,3 @@
+*/ in this we will talk about the primitive and the non primitive data types , how they work like array
+float , intgere etc and the memory management which is alos of two types , primitve and the non 
+primitive , orimitive is stack and the non primitive is heap memory 
