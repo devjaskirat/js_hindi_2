832d0c21e425e805dea27cda8a06bd91a4a74b59
devjaskirat <ramghariajaskirat608@gmail.com>
2024-10-10 13:12:27 +0530

    Create 04_objects.js

---
+++ .devcontainer/04_objects.js
@@ -0,0 +1,69 @@
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name ="jaskirat"
+tinderUser.isLoggedIn = true
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname: {
+        userFullname:
+        {
+            firstname: "jaskirat",
+            lastname: " singh"
+        }
+    }
+}
+// console.log(regularUser.fullname?.userFullname);
+// here e add the question amrk to check is the regularuser.fullname exist or not 
+// because if i do not use this , i have to apply the conditional loos
+// jitni marji nesting kr sakte ho 
+
+const obj1 = {1: " a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = { obj1, obj2}
+// display the object inside th object
+
+
+// const obj3 = Object.assign({},obj1,obj2);
+
+// here the {} act as the target and the obj1, obj2 act as the source 
+
+// const obj3 = Object.assign(target,source); the actual original syntax
+const Obj3 = {...obj1,  ...obj2}
+ console.log(Obj3);
+
+ const Users = [
+  {
+    id:1,
+    email: "jsingh@google.com"
+   
+  }, 
+  //  the comma separate the different objects in the array of the objects 
+  {
+  },
+  {},
+  {},
+]
+Users[1].email;
+//  console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+
+// console.log(Object.values(tinderUser));
+
+// console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
+
+
+
+
+
+
+
+
+
